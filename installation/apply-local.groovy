b5d9e2a1a7a4ad31b7b8d1549ac001cc3d65d4c6
Eric Berry <elberry@gmail.com>
2012-12-13 14:30:09 -0800

    moving version into gradle.properties and fixing bug #1085689

--- installation/apply-local.groovy
+++ installation/apply-local.groovy
@@ -1,13 +1,16 @@
 buildscript {
 	repositories {
 		ivy {
-		   String tpInstallDir = 'file:///Users/elberry/development/projects/mine/[organization]'
+		   String tpInstallDir = project.hasProperty('gradle.templates.dir') ? project.getProperty('gradle.templates.dir') :
+				                                                         System.properties['user.dir']
 			name = 'gradle_templates'
 			artifactPattern "${tpInstallDir}/build/libs/[artifact]-[revision].jar"
 		}
 	}
 	dependencies {
-		classpath 'gradle-templates:templates:1.2'
+		def templatesVersion = project.hasProperty('gradle.templates.ver') ? project.getProperty('gradle.templates.ver') :
+                                                                         project.version
+		classpath "gradle-templates:templates:${templatesVersion}"
 	}
 }
 // Check to make sure templates.TemplatesPlugin isn't already added.
